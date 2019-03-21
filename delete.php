2431375e3a425e54672ce485a5b8a7822623ae1b
Bankirov Aleksey <bankirovaleksey@gmail.com>
2019-03-21 17:19:55 +0300

    Удаление задачи

---
+++ delete.php
@@ -0,0 +1,28 @@
+<?php
+
+include_once 'db_connect.php';
+if (!isset($_SESSION['username'])){
+    header("Location: login-form.php");
+    exit();
+} else {
+    if (!isset($_GET['id'])){
+        header("Location: index.php");
+        exit();
+    } else {
+        $id = htmlspecialchars(rtrim($_GET['id']));
+        if ($id == ""){
+            header("Location: index.php");
+            exit();
+        }
+        if (!is_numeric($id)){
+            header("Location: index.php");
+            exit();
+        }
+        $sql = "DELETE FROM articles WHERE id=:id";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([':id' => $id]);
+        header("Location: index.php");
+        exit();
+    }
+}
+
